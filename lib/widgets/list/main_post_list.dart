0fbbac6719f71e522f00131c69e8fbfcfcbfcdd9
Jean-Charles Moussé <jeancharles.msse@gmail.com>
2019-05-01 18:30:31 +0200

    Add refresh indicator on mainlist

--- lib/widgets/list/main_post_list.dart
+++ lib/widgets/list/main_post_list.dart
@@ -2,15 +2,32 @@ import 'package:discover/widgets/post/post_row.dart';
 import 'package:flutter/material.dart';
 
 class MainPostList extends StatefulWidget {
+  MainPostList({Key key}) : super(key: key);
+  
   @override
-  _MainPostListState createState() => _MainPostListState();
+  MainPostListState createState() => MainPostListState();
 }
 
-class _MainPostListState extends State<MainPostList> {
+class MainPostListState extends State<MainPostList> {
+  final ScrollController _controller = ScrollController();
+
+  Future _fetchData() async {
+    await Future.delayed(Duration(seconds: 1));
+  }
+
+  Future<void> goToTop() {
+    return _controller.animateTo(0,
+        curve: Curves.easeIn, duration: Duration(milliseconds: 300));
+  }
+
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemBuilder: (_, __) => PostRow(),
+    return RefreshIndicator(
+      onRefresh: _fetchData,
+      child: ListView.builder(
+        controller: _controller,
+        itemBuilder: (_, __) => PostRow(),
+      ),
     );
   }
 }
