95f5b3d20d8b3058cdd03bafb86775a4d54af02e
Jean-Charles Moussé <jeancharles.msse@gmail.com>
2019-05-13 09:10:02 +0200

    Add sort by distance

--- lib/models/posts/sort_mode.dart
+++ lib/models/posts/sort_mode.dart
@@ -2,24 +2,26 @@ class SortMode {
   final String value;
   const SortMode._(this.value);
 
+  static const DISTANCE = SortMode._('distance');
   static const DATE = SortMode._('date_post');
   static const LIKES = SortMode._('likes_post');
   static const COMMENTS = SortMode._('comments_post');
 
-  static List<SortMode> get values => [DATE, LIKES, COMMENTS];
+  static List<SortMode> get values => [DISTANCE, DATE, LIKES, COMMENTS];
 
   static SortMode fromValue(String value) {
     return values.firstWhere(
       (r) => r.value.toLowerCase() == value?.toLowerCase(),
-      orElse: () => DATE,
+      orElse: () => DISTANCE,
     );
   }
 
   @override
   String toString() {
+    if (this == DATE) return "date";
     if (this == COMMENTS) return "comments";
-    if (this == LIKES) return "likes";
-    return "date";
+    if (this == LIKES) return "most liked";
+    return "distance";
   }
 
   @override
